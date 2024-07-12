af9d6263300be919ec5be405ae12a369685d203f
Mike Khor <khorenghoe@gmail.com>
2024-07-12 19:09:34 +0800

    helper functions, clean

--- reco_analysis/reco_analysis/summarizer_app/data_type.py
+++ reco_analysis/reco_analysis/summarizer_app/data_type.py
@@ -12,8 +12,11 @@ class VitalSigns:
     blood_pressure_diastolic: float | None
     weight: float | None
 
+    def to_dict(self) -> dict:
+        return dataclasses.asdict(self)
+
     def to_json(self) -> str:
-        return json.dumps(dataclasses.asdict(self))
+        return json.dumps(self.to_dict())
 
 
 @dataclasses.dataclass
@@ -24,5 +27,26 @@ class TranscriptSummary:
     current_medications: str
     summary: str
 
+    @staticmethod
+    def from_dict(data: dict) -> "TranscriptSummary":
+        return TranscriptSummary(
+            patient_overview=data["patient_overview"],
+            current_symptoms=data["current_symptoms"],
+            vital_signs=VitalSigns(
+                temperature=data["vital_signs"]["temperature"],
+                heart_rate=data["vital_signs"]["heart_rate"],
+                respiratory_rate=data["vital_signs"]["respiratory_rate"],
+                oxygen_saturation=data["vital_signs"]["oxygen_saturation"],
+                blood_pressure_systolic=data["vital_signs"]["blood_pressure_systolic"],
+                blood_pressure_diastolic=data["vital_signs"]["blood_pressure_diastolic"],
+                weight=data["vital_signs"]["weight"],
+            ),
+            current_medications=data["current_medications"],
+            summary=data["summary"],
+        )
+
+    def to_dict(self) -> dict:
+        return dataclasses.asdict(self)
+
     def to_json(self) -> str:
-        return json.dumps(dataclasses.asdict(self))
+        return json.dumps(self.to_dict())

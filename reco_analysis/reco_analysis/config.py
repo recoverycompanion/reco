d44c075fd5de18f6497dba1bfa6d8302a35e73f1
Gary Kong <garykong@berkeley.edu>
2024-07-24 10:04:14 +0700

    Updated handling of filepaths to return tuple of paths and reference config.py

--- reco_analysis/reco_analysis/config.py
+++ reco_analysis/reco_analysis/config.py
@@ -1,19 +1,19 @@
 from pathlib import Path
-
+from typing import Literal
 from dotenv import load_dotenv
 from loguru import logger
 
 # Load environment variables from .env file if it exists
 load_dotenv()
 
-# Paths
+# Directory paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
 TRANSCRIPTS_DIR = DATA_DIR / "transcripts"
 SUMMARIES_DIR = DATA_DIR / "summaries"
-EVALUATION_DIR = DATA_DIR / "evaluation"
+EVALUATION_DIR = DATA_DIR / "evaluations"
 TRANSCRIPTS_EVALUATION_DIR = EVALUATION_DIR / "transcripts"
 SUMMARIES_EVALUATION_DIR = EVALUATION_DIR / "summaries"
 RAW_DATA_DIR = DATA_DIR / "raw"
